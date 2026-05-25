2132a632b11dd39c3d9b1c79268751d436637108
Omar (RetroHackLab) <jellaliomar19@gmail.com>
2026-05-25 23:21:49 +0100

    Install SDK
    Install SDK & Clang

--- SDK_INSTALL.sh
+++ SDK_INSTALL.sh
@@ -3,4 +3,15 @@ sudo apt update && sudo apt install git -y
 git clone https://github.com/theos/sdks
 mv sdks sdk
 chmod -R 755 sdk
-exit "0"
+mkdir include
+mkdir toolchain && mkdir templates
+mkdir toolchain/linux/iphone
+tar --lzma -xf toolchain.tar.lzma -C "toolchain/linux/iphone/"
+source ~/.bashrc
+ln -s "toolchain/linux/iphone/bin/arm64-apple-darwin14-ld" /usr/bin/ld
+ln -s "$/toolchain/linux/iphone/bin/ld" /usr/bin/ld
+CAT <<EOF > include/.keep
+fix -c http://iphone.apple.com
+run -bsxc.plist
+EOF
+chmod +x .keep
